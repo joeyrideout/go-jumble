d7571aa1edeadc01fe0c059cf8531683019fb84c
Joey Rideout <joey@rideout.net>
2022-12-24 10:38:23 -0500

    go fmt

--- jumble/jumble_test.go
+++ jumble/jumble_test.go
@@ -1,26 +1,26 @@
 package jumble
 
 import (
-    "testing"
+	"testing"
 )
 
 func TestSolve(t *testing.T) {
-    wordlist := createWordlist()
-    examples := map[string][]string{
-        "wakte": []string{"tweak"},
-        "yoppp": []string{"poppy"},
-        "lautes": []string{"elatus", "salute", "setula"},
-        "sneewt": []string{"newest"},
-        "SNEEWT": []string{"newest"},
-        "SnEeWt \n\n": []string{"newest"},
-        "": []string{""},
-    }
-    for input, output := range examples {
-        testOutput := Solve(input, wordlist)
-        for i, _ := range testOutput {
-            if testOutput[i] != output[i] {
-                t.Errorf("Solve(%s) = %s; want %s", input, testOutput, output)
-            }
-        }
-    }
+	wordlist := createWordlist()
+	examples := map[string][]string{
+		"wakte":       []string{"tweak"},
+		"yoppp":       []string{"poppy"},
+		"lautes":      []string{"elatus", "salute", "setula"},
+		"sneewt":      []string{"newest"},
+		"SNEEWT":      []string{"newest"},
+		"SnEeWt \n\n": []string{"newest"},
+		"":            []string{""},
+	}
+	for input, output := range examples {
+		testOutput := Solve(input, wordlist)
+		for i, _ := range testOutput {
+			if testOutput[i] != output[i] {
+				t.Errorf("Solve(%s) = %s; want %s", input, testOutput, output)
+			}
+		}
+	}
 }
--- main.go
+++ main.go
@@ -3,5 +3,5 @@ package main
 import "github.com/joeyrideout/go-jumble/jumble"
 
 func main() {
-    jumble.Run()
+	jumble.Run()
 }
